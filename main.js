770a5f408e0325fc661f30ecdfee4c500c50c8c0
jjfreeburn <118950347+jjfreeburn@users.noreply.github.com>
2022-12-20 16:10:05 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+// Set myAge to any human age
+const myAge = 39;
+
+// Save value 2 to earlyYear
+var earlyYears = 2;
+
+earlyYears *= 10.5;
+
+//Subtract 2 from the current age
+var laterYears = myAge - 2;
+
+// Combine the two variables to get age in dog year
+var myAgeInDogYears = earlyYears + laterYears;
+
+// assign your name
+var myName = "JJ";
+
+// display the sentence and make your name to be in lowercase. Add the calculate values into the sentence.
+console.log(
+  `My name is ${myName.toLowerCase()}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`
+);
